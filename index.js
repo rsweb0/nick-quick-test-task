4fe75dfa550ee8cbf3c25319a8a7b4ab4d716e9b
Rohit <rswebstar267@gmail.com>
2020-06-18 20:22:39 +0530

    Added little space between code

--- index.js
+++ index.js
@@ -17,26 +17,35 @@ const run = async () => {
   switch (result.operation) {
     case 'List All Files':
       const allFiles = await listAllFiles()
+
       if (!allFiles.length) {
         return console.log(chalk.red('No Files Found'))
       }
+
       console.log(chalk.green('Below is the list of all files'))
       console.table(allFiles, ['Key', 'ETag', 'LastModified'])
       break
+
     case 'Upload A File':
       const { filePath } = await inquirer.uploadFile()
+
       const fileUrl = await uploadFile(filePath)
+
       console.log(chalk.green(`File Uploaded Successfully URL: ${fileUrl}`))
       break
+
     case 'Find Files':
       const findFilesAnswer = await inquirer.findFiles()
 
       const filteredFiles = await findFiles(new RegExp(findFilesAnswer.regex))
+
       if (!filteredFiles.length) {
         return console.log(chalk.red('No Files Found'))
       }
+
       console.log(chalk.green('Below is the list all matched files'))
       console.table(filteredFiles, ['Key', 'ETag', 'LastModified'])
+
       break
     case 'Find And Delete Files':
       const findAndDeleteFilesAnswer = await inquirer.findFiles()
@@ -46,8 +55,10 @@ const run = async () => {
       if (!deletedFiles.length) {
         return console.log(chalk.red('No Files Deleted'))
       }
+
       console.log(chalk.green('Below is the list of deleted files'))
       console.table(deletedFiles, ['Key'])
+
       break
   }
 }
